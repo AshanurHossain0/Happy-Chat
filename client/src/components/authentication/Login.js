a2a545ec07a04b37b2349d162d74810beb2ed6d4
AshanurHossain0 <ashanurhossain0@gmail.com>
2023-08-23 14:59:14 +0530

    login&signup_functionality

--- client/src/components/authentication/Login.js
+++ client/src/components/authentication/Login.js
@@ -1,12 +1,67 @@
-import React, { useState } from 'react'
-import { VStack, FormControl, FormLabel, Input, InputGroup, InputRightElement, Button } from '@chakra-ui/react'
+import React, { useState } from 'react';
+import axios from 'axios';
+import {useToast, VStack, FormControl, FormLabel, Input, InputGroup, InputRightElement, Button } from '@chakra-ui/react'
+import {useHistory} from 'react-router-dom'
 
 const Login = () => {
     const [show, setShow] = useState(false);
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [loading, setLoading] = useState(false);
 
-    const handleSubmit=()=>{console.log(email,password)}
+    const toast=useToast();
+    const history=useHistory();
+
+    const handleSubmit = async () => {
+        setLoading(true);
+        if (!email || !password) {
+            toast({
+                title: "Please Fill all the Feilds",
+                status: "warning",
+                duration: 4000,
+                isClosable: true,
+                position: "top",
+            });
+            setLoading(false);
+            return;
+        }
+
+        try {
+            const config = {
+                headers: {
+                    "Content-type": "application/json",
+                },
+            };
+
+            const { data } = await axios.post(
+                `${process.env.REACT_APP_BASEURL}/api/user/login`,
+                { email, password },
+                config
+            );
+
+            toast({
+                title: "Login Successful",
+                status: "success",
+                duration: 4000,
+                isClosable: true,
+                position: "top",
+            });
+            // setUser(data);
+            localStorage.setItem("userInfo", JSON.stringify(data));
+            setLoading(false);
+            history.push("/chats");
+        } catch (error) {
+            toast({
+                title: "Error Occured!",
+                description: error.response.data.message,
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position: "top",
+            });
+            setLoading(false);
+        }
+    }
 
     return (
         <VStack spacing='5px'>
@@ -15,19 +70,21 @@ const Login = () => {
                 <Input
                     placeholder='Enter Your Email'
                     onChange={(e) => setEmail(e.target.value)}
+                    value={email}
                 />
             </FormControl>
             <FormControl id='password2' isRequired>
                 <FormLabel>Password</FormLabel>
                 <InputGroup>
                     <Input
-                        type={show?"text":"password"}
+                        type={show ? "text" : "password"}
                         placeholder='Enter Your Password'
+                        value={password}
                         onChange={(e) => setPassword(e.target.value)}
                     />
                     <InputRightElement w='4.5rem'>
-                        <Button h='1.75rem' size='sm' onClick={()=>setShow(!show)}>
-                            {show?"Hide":"Show"}
+                        <Button h='1.75rem' size='sm' onClick={() => setShow(!show)}>
+                            {show ? "Hide" : "Show"}
                         </Button>
                     </InputRightElement>
                 </InputGroup>
@@ -35,8 +92,9 @@ const Login = () => {
             <Button
                 colorScheme='blue'
                 w='100%'
-                style={{marginTop:15}}
+                style={{ marginTop: 15 }}
                 onClick={handleSubmit}
+                isLoading={loading}
             >
                 Login
             </Button>
@@ -44,7 +102,7 @@ const Login = () => {
                 variant='solid'
                 colorScheme='red'
                 w='100%'
-                onClick={()=>{
+                onClick={() => {
                     setEmail('guest@example.com')
                     setPassword("123456")
                 }}
--- client/src/components/authentication/Signup.js
+++ client/src/components/authentication/Signup.js
@@ -1,17 +1,123 @@
-import React, { useState } from 'react'
-import { VStack, FormControl, FormLabel, Input, InputGroup, InputRightElement, Button } from '@chakra-ui/react'
+import React, { useState} from 'react'
+import axios from 'axios';
+import {useHistory} from 'react-router-dom'
+import { useToast, VStack, FormControl, FormLabel, Input, InputGroup, InputRightElement, Button } from '@chakra-ui/react'
 
 const Signup = () => {
 
     const [show, setShow] = useState(false);
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmpassword, setConfirmPassword] = useState('');
-    const [pic, setPic] = useState('');
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [confirmpassword, setConfirmPassword] = useState();
+    const [pic, setPic] = useState();
+    const [loading, setLoading] = useState(false);
+    const history=useHistory();
 
-    const postPic=()=>{}
-    const handleSubmit=()=>{console.log(name,email,password,confirmpassword)}
+    const toast = useToast();
+
+    const postPic = (pic) => {
+        setLoading(true);
+        if (pic === undefined) {
+            toast({
+                title: 'Select an image',
+                status: 'warning',
+                duration: 4000,
+                isClosable: true,
+                position:"top"
+            })
+            return;
+        }
+        if (pic.type === "image/jpeg" || pic.type === "image/png") {
+            const data = new FormData();
+            data.append('file', pic);
+            data.append('folder', 'happy-to-chat');
+            data.append('upload_preset', 'happy-to-chat');
+            data.append('cloud_name', 'dglwbgm9j');
+            fetch(`https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDNAME}/image/upload`, {
+                method: 'post',
+                body: data
+            }).then(res => res.json())
+                .then(data => {
+                    setPic(data.url.toString());
+                    setLoading(false);
+                    return;
+                })
+                .catch(err => {
+                    console.log(err);
+                    setLoading(false);
+                    return;
+                })
+        }
+        else {
+            toast({
+                title: 'Select an image',
+                status: 'warning',
+                duration: 4000,
+                isClosable: true,
+                position:"top"
+            })
+            return;
+        }
+    }
+    const handleSubmit = async () => {
+        setLoading(true);
+        if (!name || !email || !password || !confirmpassword) {
+            toast({
+                title: "Please Fill all the Feilds",
+                status: "warning",
+                duration: 5000,
+                isClosable: true,
+                position:"top"
+            });
+            setLoading(false);
+            return;
+        }
+        if (password !== confirmpassword) {
+            toast({
+                title: "Passwords Do Not Match",
+                status: "warning",
+                duration: 5000,
+                isClosable: true,
+                position:"top"
+            });
+            setLoading(false);
+            return;
+        }
+        try {
+            const config = {
+                headers: {
+                    "Content-type": "application/json",
+                },
+            };
+            const { data } = await axios.post(
+                `${process.env.REACT_APP_BASEURL}/api/user`,
+                {name,email,password,pic},
+                config
+            );
+            console.log(data);
+            toast({
+                title: "Registration Successful",
+                status: "success",
+                duration: 5000,
+                isClosable: true,
+                position:"top"
+            });
+            localStorage.setItem("userInfo", JSON.stringify(data));
+            setLoading(false);
+            history.push("/chats");
+        } catch (error) {
+            toast({
+                title: "Error Occured!",
+                description: error.response.data.message,
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position:"top"
+            });
+            setLoading(false);
+        }
+    }
 
     return (
         <VStack spacing='5px'>
@@ -33,13 +139,13 @@ const Signup = () => {
                 <FormLabel>Password</FormLabel>
                 <InputGroup>
                     <Input
-                        type={show?"text":"password"}
+                        type={show ? "text" : "password"}
                         placeholder='Enter Your Password'
                         onChange={(e) => setPassword(e.target.value)}
                     />
                     <InputRightElement w='4.5rem'>
-                        <Button h='1.75rem' size='sm' onClick={()=>setShow(!show)}>
-                            {show?"Hide":"Show"}
+                        <Button h='1.75rem' size='sm' onClick={() => setShow(!show)}>
+                            {show ? "Hide" : "Show"}
                         </Button>
                     </InputRightElement>
                 </InputGroup>
@@ -48,13 +154,13 @@ const Signup = () => {
                 <FormLabel>Confirm Password</FormLabel>
                 <InputGroup>
                     <Input
-                        type={show?"text":"password"}
+                        type={show ? "text" : "password"}
                         placeholder='Confirm Password'
                         onChange={(e) => setConfirmPassword(e.target.value)}
                     />
                     <InputRightElement w='4.5rem'>
-                        <Button h='1.75rem' size='sm' onClick={()=>setShow(!show)}>
-                            {show?"Hide":"Show"}
+                        <Button h='1.75rem' size='sm' onClick={() => setShow(!show)}>
+                            {show ? "Hide" : "Show"}
                         </Button>
                     </InputRightElement>
                 </InputGroup>
@@ -71,8 +177,9 @@ const Signup = () => {
             <Button
                 colorScheme='blue'
                 w='100%'
-                style={{marginTop:15}}
+                style={{ marginTop: 15 }}
                 onClick={handleSubmit}
+                isLoading={loading}
             >
                 Sign Up
             </Button>
