0f0f93dcfac213ebaae5a6246bd17c7eb4abcce4
AshanurHossain0 <ashanurhossain0@gmail.com>
2023-08-30 13:22:02 +0530

    otp

--- client/src/components/authentication/Signup.js
+++ client/src/components/authentication/Signup.js
@@ -1,6 +1,6 @@
-import React, { useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios';
-import { useToast, VStack, FormControl, FormLabel, Input, InputGroup, InputRightElement, Button } from '@chakra-ui/react'
+import { useToast, VStack, FormControl, FormLabel, Input, InputGroup, InputRightElement, Button,Text } from '@chakra-ui/react'
 
 const Signup = () => {
 
@@ -11,6 +11,9 @@ const Signup = () => {
     const [confirmpassword, setConfirmPassword] = useState('');
     const [pic, setPic] = useState('');
     const [loading, setLoading] = useState(false);
+    const [otp,setOtp]=useState('');
+    const [isVerifying, setIsVerifying] = useState(false);
+    const [timeLeft,setTimeLeft]=useState(160);
 
     const toast = useToast();
 
@@ -22,7 +25,7 @@ const Signup = () => {
                 status: 'warning',
                 duration: 4000,
                 isClosable: true,
-                position:"top"
+                position: "top"
             })
             return;
         }
@@ -53,7 +56,7 @@ const Signup = () => {
                 status: 'warning',
                 duration: 4000,
                 isClosable: true,
-                position:"top"
+                position: "top"
             })
             return;
         }
@@ -66,7 +69,7 @@ const Signup = () => {
                 status: "warning",
                 duration: 5000,
                 isClosable: true,
-                position:"top"
+                position: "top"
             });
             setLoading(false);
             return;
@@ -75,9 +78,9 @@ const Signup = () => {
             toast({
                 title: "Passwords Do Not Match",
                 status: "warning",
-                duration: 5000,
+                duration: 4000,
                 isClosable: true,
-                position:"top"
+                position: "top"
             });
             setLoading(false);
             return;
@@ -90,17 +93,11 @@ const Signup = () => {
             };
             const { data } = await axios.post(
                 `${process.env.REACT_APP_BASEURL}/api/user`,
-                {name,email,password,pic},
+                { name, email, password, pic },
                 config
             );
-            toast({
-                title: "Registration Successful",
-                status: "success",
-                duration: 5000,
-                isClosable: true,
-                position:"top"
-            });
             setLoading(false);
+            setIsVerifying(true);
         } catch (error) {
             toast({
                 title: "Error Occured!",
@@ -108,77 +105,178 @@ const Signup = () => {
                 status: "error",
                 duration: 5000,
                 isClosable: true,
-                position:"top"
+                position: "top"
             });
             setLoading(false);
         }
     }
 
+    const handleSubmitOtp= async ()=>{
+        setLoading(true);
+        if (otp==0) {
+            toast({
+                title: "Please fill the otp field",
+                status: "warning",
+                duration: 4000,
+                isClosable: true,
+                position: "top"
+            });
+            setLoading(false);
+            return;
+        }
+        try {
+            const config = {
+                headers: {
+                    "Content-type": "application/json",
+                },
+            };
+            const { data } = await axios.post(
+                `${process.env.REACT_APP_BASEURL}/api/user/verify`,
+                { email,otp },
+                config
+            );
+            setIsVerifying(false);
+            setLoading(false);
+            setOtp()
+            toast({
+                title: "Registration Successful",
+                status: "success",
+                duration: 5000,
+                isClosable: true,
+                position: "top"
+            });
+            
+        } catch (error) {
+            toast({
+                title: "Error Occured!",
+                description: error.response.data.message,
+                status: "error",
+                duration: 4000,
+                isClosable: true,
+                position: "top"
+            });
+            setOtp();
+            setLoading(false);
+        }
+
+    }
+
+    useEffect(()=>{
+        if(isVerifying){
+            const intId=setInterval(()=>{
+                if(timeLeft>0){
+                    setTimeLeft(time=>time-1);
+                }
+                if(timeLeft<=0){
+                    setIsVerifying(false);
+                    clearInterval(intId);
+                }
+            },1000)
+
+            return ()=>{clearInterval(intId)};
+        }
+    },[isVerifying,timeLeft])
+
     return (
-        <VStack spacing='5px'>
-            <FormControl id='full-name' isRequired>
-                <FormLabel>Name</FormLabel>
-                <Input
-                    placeholder='Enter Your Name'
-                    onChange={(e) => setName(e.target.value)}
-                />
-            </FormControl>
-            <FormControl id='email' isRequired>
-                <FormLabel>Email</FormLabel>
-                <Input
-                    placeholder='Enter Your Email'
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-            </FormControl>
-            <FormControl id='password' isRequired>
-                <FormLabel>Password</FormLabel>
-                <InputGroup>
-                    <Input
-                        type={show ? "text" : "password"}
-                        placeholder='Enter Your Password'
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <InputRightElement w='4.5rem'>
-                        <Button h='1.75rem' size='sm' onClick={() => setShow(!show)}>
-                            {show ? "Hide" : "Show"}
+        <div>
+            {
+                !isVerifying ? (
+                    <VStack spacing='5px'>
+                        <FormControl id='full-name' isRequired>
+                            <FormLabel>Name</FormLabel>
+                            <Input
+                                value={name}
+                                placeholder='Enter Your Name'
+                                onChange={(e) => setName(e.target.value)}
+                            />
+                        </FormControl>
+                        <FormControl id='email' isRequired>
+                            <FormLabel>Email</FormLabel>
+                            <Input
+                                value={email}
+                                placeholder='Enter Your Email'
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </FormControl>
+                        <FormControl id='password' isRequired>
+                            <FormLabel>Password</FormLabel>
+                            <InputGroup>
+                                <Input
+                                    value={password}
+                                    type={show ? "text" : "password"}
+                                    placeholder='Enter Your Password'
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                                <InputRightElement w='4.5rem'>
+                                    <Button h='1.75rem' size='sm' onClick={() => setShow(!show)}>
+                                        {show ? "Hide" : "Show"}
+                                    </Button>
+                                </InputRightElement>
+                            </InputGroup>
+                        </FormControl>
+                        <FormControl id='confirm-password' isRequired>
+                            <FormLabel>Confirm Password</FormLabel>
+                            <InputGroup>
+                                <Input
+                                    value={confirmpassword}
+                                    type={show ? "text" : "password"}
+                                    placeholder='Confirm Password'
+                                    onChange={(e) => setConfirmPassword(e.target.value)}
+                                />
+                                <InputRightElement w='4.5rem'>
+                                    <Button h='1.75rem' size='sm' onClick={() => setShow(!show)}>
+                                        {show ? "Hide" : "Show"}
+                                    </Button>
+                                </InputRightElement>
+                            </InputGroup>
+                        </FormControl>
+                        <FormControl id='pic'>
+                            <FormLabel>Upload Profile Picture</FormLabel>
+                            <Input
+                                type='file'
+                                p='1.5'
+                                accept='image/*'
+                                onChange={(e) => postPic(e.target.files[0])}
+                            />
+                        </FormControl>
+                        <Button
+                            colorScheme='blue'
+                            w='100%'
+                            style={{ marginTop: 15 }}
+                            onClick={handleSubmit}
+                            isLoading={loading}
+                        >
+                            Sign Up
                         </Button>
-                    </InputRightElement>
-                </InputGroup>
-            </FormControl>
-            <FormControl id='confirm-password' isRequired>
-                <FormLabel>Confirm Password</FormLabel>
-                <InputGroup>
-                    <Input
-                        type={show ? "text" : "password"}
-                        placeholder='Confirm Password'
-                        onChange={(e) => setConfirmPassword(e.target.value)}
-                    />
-                    <InputRightElement w='4.5rem'>
-                        <Button h='1.75rem' size='sm' onClick={() => setShow(!show)}>
-                            {show ? "Hide" : "Show"}
+                    </VStack>
+                ) : (
+                    <VStack spacing='5px'>
+                        <FormControl id='otp' isRequired>
+                            <FormLabel>OTP</FormLabel>
+                            <Input
+                                value={otp}
+                                placeholder='Enter OTP'
+                                onChange={(e) => setOtp(e.target.value)}
+                            />
+                        </FormControl>
+                        {
+                            isVerifying && <Text color='green' >Time Left : {timeLeft}s</Text>
+                        }
+                        
+                        <Button
+                            isDisabled={loading}
+                            colorScheme='blue'
+                            w='100%'
+                            style={{ marginTop: 15 }}
+                            onClick={handleSubmitOtp}
+                            isLoading={loading}
+                        >
+                            Verify OTP
                         </Button>
-                    </InputRightElement>
-                </InputGroup>
-            </FormControl>
-            <FormControl id='pic'>
-                <FormLabel>Upload Profile Picture</FormLabel>
-                <Input
-                    type='file'
-                    p='1.5'
-                    accept='image/*'
-                    onChange={(e) => postPic(e.target.files[0])}
-                />
-            </FormControl>
-            <Button
-                colorScheme='blue'
-                w='100%'
-                style={{ marginTop: 15 }}
-                onClick={handleSubmit}
-                isLoading={loading}
-            >
-                Sign Up
-            </Button>
-        </VStack>
+                    </VStack>
+                )
+            }
+        </div>
     )
 }
 
